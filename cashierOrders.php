cdbd2290a8b696102bf8a99c898fd3efd673652b
Harveth <georgedaem@gmail.com>
2022-06-12 01:35:56 +0200

    made slight change

--- cashierOrders.php
+++ cashierOrders.php
@@ -11,7 +11,6 @@
     $conn = $db->getConnection();
 
     $data = json_decode(file_get_contents("php://input"));
-    $req = $data->req;
     //echo "$req";
 
     $query = "SELECT orders.id, users.username, food.name from users, food, orders, order_food WHERE users.id = orders.user_id AND food.id = order_food.foodId AND order_food.orderId = orders.id;";
@@ -27,13 +26,8 @@
         $orders[$counter++] = $cur;
     }
 
-    if($orders = NULL){
-        http_response_code(200);
-        echo json_encode("error");
-    } else {
-        http_response_code(200);
-        echo json_encode($orders);
-    }
+    http_response_code(200);
+    echo json_encode($orders);
 
     // http_response_code(200);
     // echo json_encode($items);
